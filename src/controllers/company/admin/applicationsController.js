1d6fa569073351bc88905d1e73dac32a6ee45be0
Samuel <oluwafemisam40@gmail.com>
2025-01-31 05:32:12 +0100

    added company and user details to the application controller

--- src/controllers/company/admin/applicationsController.js
+++ src/controllers/company/admin/applicationsController.js
@@ -1,10 +1,23 @@
 const { Applications } = require("../../../../models");
 const { successResponse, errorResponse } = require("../../../utils/response");
 
-// Controller to list all applications
+// Controller to list all applications with company and user details
 const listApplications = async (req, res) => {
   try {
-    const applications = await Applications.findAll();
+    const applications = await Applications.findAll({
+      include: [
+        {
+          model: Companies,
+          as: "company",
+          attributes: { exclude: ["createdAt", "updatedAt"] },
+        },
+        {
+          model: Users,
+          as: "user",
+          attributes: { exclude: ["password", "createdAt", "updatedAt"] },
+        },
+      ],
+    });
 
     return successResponse(res, "Fetched Successfully", applications);
   } catch (error) {

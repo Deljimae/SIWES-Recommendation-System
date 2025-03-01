d85506c53fd544e4a1013b6015cf723ca3ae7c75
Samuel <oluwafemisam40@gmail.com>
2025-03-01 06:31:47 +0100

    removed ping.yml fime

--- src/controllers/recommendationController.js
+++ src/controllers/recommendationController.js
@@ -1,66 +1,89 @@
-const cosineSimilarity = require('../utils/cosineSimilarity'); // Import cosine similarity function (see below)
-const { User, Profile, Company } = require('../../models');
-const { errorResponse, successResponse, customResponse } = require('../utils/response');
-const { validate: uuidValidate } = require('uuid');
-const getEmbeddingsFromPythonService = require('../services/embeddingPYService');
+const cosineSimilarity = require("../utils/cosineSimilarity"); // Import cosine similarity function (see below)
+const { User, Profile, Company } = require("../../models");
+const {
+  errorResponse,
+  successResponse,
+  customResponse,
+} = require("../utils/response");
+const { validate: uuidValidate } = require("uuid");
+const getEmbeddingsFromPythonService = require("../services/embeddingPYService");
 
-
-
-// bactch companies send
+// batch companies send
 async function generateRecommendationsForUser(req, res) {
   const { userId } = req.user;
   const isValidUUID = uuidValidate(userId);
 
   if (!isValidUUID) {
-    return errorResponse(res, 'Invalid UUID format', 400);
+    return errorResponse(res, "Invalid UUID format", 400);
   }
 
-  console.log('Generating recommendations for user:', userId);
+  console.log("Generating recommendations for user:", userId);
 
   try {
     const user = await User.findOne({
       where: { uuid: userId },
-      include: { model: Profile, as: 'Profile' },
+      include: { model: Profile, as: "Profile" },
     });
 
     if (!user) {
       return errorResponse(res, `User with ID ${userId} not found.`, 404);
     }
 
-    if (!user.Profile || !user.Profile[0].interests || user.Profile[0].interests.length === 0) {
+    if (
+      !user.Profile ||
+      !user.Profile[0].interests ||
+      user.Profile[0].interests.length === 0
+    ) {
       console.warn(`User ${user.uuid} has not updated their interest.`);
       // return res.status(200).json({ message: "User has no career goals.", recommendations: [], user });
-      return successResponse(res, "Please update your interests in the profile section", { recommendations: [] })
+      return successResponse(
+        res,
+        "Please update your interests in the profile section",
+        { recommendations: [] }
+      );
     }
 
     const companies = await Company.findAll();
 
     if (!companies || companies.length === 0) {
       console.warn("No companies found.");
-      return successResponse(res, "No companies found.", { recommendations: [] })
+      return successResponse(res, "No companies found.", {
+        recommendations: [],
+      });
     }
 
     let combinedGoals = user.Profile[0].interests.join(" ");
 
-    if (user.Profile || user.Profile[0].career_goals || user.Profile[0].career_goals.length !== 0) {
+    if (
+      user.Profile ||
+      user.Profile[0].career_goals ||
+      user.Profile[0].career_goals.length !== 0
+    ) {
       combinedGoals += " " + user.Profile[0].career_goals.join(" ");
     }
 
     let userEmbedding;
     try {
-      const embeddingsData = await getEmbeddingsFromPythonService(combinedGoals);
+      const embeddingsData = await getEmbeddingsFromPythonService(
+        combinedGoals
+      );
       userEmbedding = embeddingsData.embeddings;
     } catch (embeddingError) {
-      console.error(`Error getting user embedding for user ${user.uuid}:`, embeddingError);
+      console.error(
+        `Error getting user embedding for user ${user.uuid}:`,
+        embeddingError
+      );
       return errorResponse(res, "Error getting user embedding.", 500);
     }
 
     // Batch company embeddings
-    const companyIndustries = companies.map(company => company.industry);
+    const companyIndustries = companies.map((company) => company.industry);
     let companyEmbeddingsData;
 
     try {
-      companyEmbeddingsData = await getEmbeddingsFromPythonService(companyIndustries);
+      companyEmbeddingsData = await getEmbeddingsFromPythonService(
+        companyIndustries
+      );
     } catch (embeddingError) {
       console.error("Error getting company embeddings:", embeddingError);
       return errorResponse(res, "Error getting company embeddings.", 500);
@@ -68,7 +91,8 @@ async function generateRecommendationsForUser(req, res) {
 
     const company_embeddings = {};
     for (let i = 0; i < companies.length; i++) {
-      company_embeddings[companies[i].uuid] = companyEmbeddingsData.embeddings[i];
+      company_embeddings[companies[i].uuid] =
+        companyEmbeddingsData.embeddings[i];
     }
 
     const recommendations = [];
@@ -98,13 +122,15 @@ async function generateRecommendationsForUser(req, res) {
     const ranked_companies = similarities.sort((a, b) => b.score - a.score);
     recommendations.push({ user_uuid: user.uuid, ranked_companies });
 
-    return successResponse(res, "Recommendations generated successfully.", { recommendations })
-
+    return successResponse(res, "Recommendations generated successfully.", {
+      recommendations,
+    });
   } catch (error) {
     console.error("Error generating recommendations:", error);
-    return customResponse(res, 500, false, 'An unexpected error occurred.', { error: error.message })
+    return customResponse(res, 500, false, "An unexpected error occurred.", {
+      error: error.message,
+    });
   }
 }
-
 
 module.exports = generateRecommendationsForUser;
